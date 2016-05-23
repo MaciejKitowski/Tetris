0c659f8da830669011bbac98dc4ff328b679302c
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-05-23 09:52:59 +0200

    Load arena tiles to array

---
+++ Assets/Arena/arenaController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arenaController : MonoBehaviour
+{
+    public GameObject[,] arenaTile = new GameObject[10, 20];
+
+    void Awake()
+    {
+        char[] splitChars = { '(', ',', ')' };
+
+        for (int y = 0; y < 20; ++y)
+        {
+            foreach (Transform obj in transform.GetChild(y).gameObject.transform)
+            {
+                string[] buffer = obj.name.Split(splitChars);
+
+                arenaTile[int.Parse(buffer[1]), y] = obj.gameObject;
+                if(Debug.isDebugBuild) Debug.Log("ArenaController - " + arenaTile[int.Parse(buffer[1]), y].name + " set to arenaTile[" + int.Parse(buffer[1]) + "," + y + "]");
+            }
+        }
+    }
+}
