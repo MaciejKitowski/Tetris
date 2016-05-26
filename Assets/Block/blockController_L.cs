028f2623c3b7b91043ad743ce0931ae7497bc5d7
Maciej Kitowski <maciejkitowski@hotmail.com>
2016-05-26 16:59:06 +0200

    [FIXED] New blocks overrides existing blocks #2
    Horizontal:
    - Block L
    - Block L (reversed)
    - Block S

--- Assets/Block/blockController_L.cs
+++ Assets/Block/blockController_L.cs
@@ -29,16 +29,18 @@ public class blockController_L : blockController
 
     override public void turnLeft()
     {
-        if ((actitveRotation == rotation.DOWN || actitveRotation == rotation.RIGHT) && tile[1].arenaTile.posX > 0) moveTilesHorizontal(-1);
-        else if (actitveRotation == rotation.UP && tile[0].arenaTile.posX > 0) moveTilesHorizontal(-1);
-        else if (actitveRotation == rotation.LEFT && tile[3].arenaTile.posX > 0) moveTilesHorizontal(-1);
+        if(actitveRotation == rotation.DOWN && canTurn(new int[3] { 1, 2, 3 }, -1)) moveTilesHorizontal(-1);
+        else if(actitveRotation == rotation.RIGHT && canTurn(new int[2] { 0, 1 }, -1)) moveTilesHorizontal(-1);
+        else if (actitveRotation == rotation.UP && canTurn(new int[1] { 0 }, -1)) moveTilesHorizontal(-1);
+        else if (actitveRotation == rotation.LEFT && canTurn(new int[1] { 3 }, -1)) moveTilesHorizontal(-1);
     }
 
     override public void turnRight()
     {
-        if (actitveRotation == rotation.DOWN && tile[0].arenaTile.posX < 9) moveTilesHorizontal(1);
-        else if (actitveRotation == rotation.RIGHT && tile[3].arenaTile.posX < 9) moveTilesHorizontal(1);
-        else if ((actitveRotation == rotation.UP || actitveRotation == rotation.LEFT) && tile[1].arenaTile.posX < 9) moveTilesHorizontal(1);
+        if (actitveRotation == rotation.DOWN && canTurn(new int[1] { 0 }, 1)) moveTilesHorizontal(1);
+        else if (actitveRotation == rotation.RIGHT && canTurn(new int[1] { 3 }, 1)) moveTilesHorizontal(1);
+        else if (actitveRotation == rotation.UP && canTurn(new int[3] { 1, 2, 3 }, 1)) moveTilesHorizontal(1);
+        else if (actitveRotation == rotation.LEFT && canTurn(new int[2] { 0, 1 }, 1)) moveTilesHorizontal(1);
     }
 
     override public void fallDown()
