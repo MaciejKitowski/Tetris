4d416b29058cc6904b3a945f5838773a5202dd2e
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-04 10:19:50 +0200

    Rotate tetrominoes

---
+++ Assets/Game/Scripts/Tetromino.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+    [SerializeField]
+    private bool rotation = true;
+
+    public void rotate() {
+        if(rotation) {
+            transform.Rotate(0, 0, 90f);
+        }
+    }
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)) rotate();
+    }
+}
