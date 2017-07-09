8f11725a1039575b16e46ee36f71ee11a1658a3f
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-09 16:23:46 +0200

    Added TetrominoSpawner class

---
+++ Assets/Game/Scripts/TetrominoSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TetrominoSpawner : MonoBehaviour {
+    [System.Serializable]
+    private struct tetromino {
+        public GameObject nextBlockUI;
+        public GameObject prefab;
+    }
+
+    [SerializeField]
+    private tetromino[] tetrominoes;
+}
