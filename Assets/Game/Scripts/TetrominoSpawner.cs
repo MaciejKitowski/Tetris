4781f14aa77534cc663de7d0a1dc42baf68a4c0b
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-09 16:43:11 +0200

    Rand new tetrominoes method

--- Assets/Game/Scripts/TetrominoSpawner.cs
+++ Assets/Game/Scripts/TetrominoSpawner.cs
@@ -9,4 +9,30 @@ public class TetrominoSpawner : MonoBehaviour {
 
     [SerializeField]
     private tetromino[] tetrominoes;
+    [SerializeField]
+    private Color[] possibleColors;
+    private int nextTetrominoID;
+    private Color nextTetrominoColor;
+
+    void Start() {
+        randNew();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) randNew();   
+    }
+
+    public void randNew() {
+        foreach(var tt in tetrominoes) {
+            tt.nextBlockUI.SetActive(false);
+        }
+
+        nextTetrominoID = Random.Range(0, tetrominoes.Length);
+        nextTetrominoColor = possibleColors[Random.Range(0, possibleColors.Length)];
+        tetrominoes[nextTetrominoID].nextBlockUI.SetActive(true);
+
+        foreach(Transform tt in tetrominoes[nextTetrominoID].nextBlockUI.transform) {
+            tt.GetComponent<SpriteRenderer>().color = nextTetrominoColor;
+        }
+    }
 }
