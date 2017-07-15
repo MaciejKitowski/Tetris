8f4cf45adad42ed90eeb279cb570f16be926ac80
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-15 14:21:09 +0200

    Added Tetromino Rotation Tile script (not used yet)

---
+++ Assets/Game/Scripts/TetrominoRotationTile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TetrominoRotationTile : MonoBehaviour {
+    private bool _canRotate = false;
+
+    public bool canRotate { get { return _canRotate; } private set { _canRotate = value; } }
+
+    void OnCollisionEnter2D(Collision2D collision) { checkRotation(collision); }
+    void OnCollisionStay2D(Collision2D collision) { checkRotation(collision); }
+    void OnCollisionExit2D(Collision2D collision) { canRotate = false; }
+
+    private void checkRotation(Collision2D collision) {
+        ArenaTile tile = collision.gameObject.GetComponent<ArenaTile>();
+        if (tile.empty) canRotate = true;
+        else canRotate = false;
+    }
+}
