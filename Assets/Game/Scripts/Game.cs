4ef68319571bd093b9f84aca611fa36850a045c9
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-17 09:45:39 +0200

    Start counting points

--- Assets/Game/Scripts/Game.cs
+++ Assets/Game/Scripts/Game.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
-
-//TODO not work yet
-public enum TetrominoFallType { DYNAMIC, STATIC } //DYNAMIC - tetromino tile will always fall down
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
-    [SerializeField]
-    private TetrominoFallType _fallype;
     [SerializeField]
     private float _tetrominoFallTime = 1.0f;
     [SerializeField]
     private float _speedUpMultiplier = 0.1f;
+    [SerializeField]
+    private Text pointsField;
+    [SerializeField]
+    private int pointsPerRow = 15;
+
+    private int points = 0;
 
     public float tetrominoFallTime { get { return _tetrominoFallTime; } private set { _tetrominoFallTime = value; } }
     public float speedUpMultiplier { get { return _speedUpMultiplier; } private set { _speedUpMultiplier = value; } }
-    public TetrominoFallType fallType { get { return _fallype; } private set { _fallype = value; } }
+
+    void Start() {
+        points = 0;
+        pointsField.text = points.ToString();
+    }
+
+    public void addPoints() {
+        points += pointsPerRow;
+        pointsField.text = points.ToString();
+    }
 }
