cd24ae75aeaf92da74ef1071ad49a5c845314737
Maciej Kitowski <MaciejKitowski@hotmail.com>
2017-07-20 09:01:09 +0200

    Separate points to new class

--- Assets/Game/Scripts/Game.cs
+++ Assets/Game/Scripts/Game.cs
@@ -4,23 +4,18 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour {
     [SerializeField] private Game_components.Tetromino _tetromino;
     [SerializeField] private Game_components.Level _level;
-
-    //TODO Separate points to different class like Game_components.Tetromino class
-    [SerializeField] private Text pointsField;
-    [SerializeField] private int pointsPerRow = 15;
-    private int points = 0;
+    [SerializeField] private Game_components.Point _points;
 
     public Game_components.Tetromino tetromino { get { return _tetromino; } }
     public Game_components.Level level { get { return _level; } }
+    public Game_components.Point points { get { return _points; } }
 
     void Start() {
-        points = 0;
-        pointsField.text = points.ToString();
+        points.setPoints(0);
     }
 
     public void addPoints() {
-        points += pointsPerRow;
+        points.addPoints();
         level.rowRemoved();
-        pointsField.text = points.ToString();
     }
 }
--- Assets/Game/Scripts/Game_Components.cs
+++ Assets/Game/Scripts/Game_Components.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game_components {
     [System.Serializable]
@@ -16,5 +17,23 @@ namespace Game_components {
         [SerializeField] private int rowsToLevel = 5;
 
         public void rowRemoved() { bar.raiseProgress(1f / rowsToLevel); }
+    }
+
+    [System.Serializable]
+    public class Point {
+        [SerializeField] private Text displayValue;
+        [SerializeField] private int pointsPerRow = 15;
+        private int _points;
+
+        public int points {
+            get { return _points; }
+            set {
+                _points = value;
+                displayValue.text = value.ToString();
+            }
+        }
+
+        public void setPoints(int val) { points = val; }
+        public void addPoints() { points += pointsPerRow; }
     }
 }
